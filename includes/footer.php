5479c2f74cdfd8507edbad40190d703d2a4644e1
Madhusmita Mukherjee <76001709+madhusmita1307@users.noreply.github.com>
2023-01-16 14:49:27 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer" >
+    <div class="container text-center">
+        <span class="text-muted">
+            <b>Copyright &copy; 2022 EazyCart | 20BCE2783 Madhusmita Mukherjee | Contact Us: +91 1234567890</b>
+        </span>
+    </div>
+</footer>
